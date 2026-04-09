fefc112a66f270c1974f2ec7cebca43662e5edb5
Adrian-Alba-01 <adrianalbasevilla01@gmail.com>
2026-04-09 21:45:48 +0200

    Implementacion de base de datos2

--- app/src/main/java/com/examp/gio/activities/MainActivity.java
+++ app/src/main/java/com/examp/gio/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.examp.gio;
+package com.examp.gio.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.examp.gio.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/examp/gio/network/ApiClient.java
+++ app/src/main/java/com/examp/gio/network/ApiClient.java
@@ -1,4 +1,4 @@
-package com.examp.gio;
+package com.examp.gio.network;
 
 import java.io.*;
 import java.net.HttpURLConnection;
